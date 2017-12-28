d6520988a69986a4188636c7848c0af6c14a3e12
fabricio.barrozo <fabricio.barrozo@srh-3363.adasa.df.gov.br>
2017-12-28 11:55:14 -0200

    Capítulo 3 - Ainda com erro .

--- src/formulariologin/LoginApp.java
+++ src/formulariologin/LoginApp.java
@@ -1,66 +1,118 @@
-
 package formulariologin;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 
                                             // cria clase para visualizacao e controle
 public class LoginApp extends Application { // esta classe se estende a um application, do javafx
-    @Override
-    public void start (Stage stage) throws Exception { // sobrescreve o método start (onde desenvolve a lógia e cria
-                                                        // componentes
-    
-    AnchorPane pane = new AnchorPane();             //  criar painel
-    pane.setPrefSize(400,300);                      // double prefWidth double prefHeight
-    
 
-    
-    TextField txtLogin = new TextField ();          // cria caixa de texto
-    txtLogin.setPromptText("Digite aqui seu login!");
-    
-    PasswordField txtSenha = new PasswordField();   // cria caixa de senha
-    txtSenha.setPromptText("Digite aqui sua senha!");
-    
-    Button btnEntrar = new Button ("Entrar");       // cria botão 
-    
-    Button btnSair = new Button ("Sair");           // cria botão
-    
-    pane.getChildren().addAll(txtLogin, txtSenha, btnEntrar, btnSair); // adicionar os componentes no painel
-    
-        Scene scene = new Scene(pane);                  // cria a cena e passa o painel 'pane'
-    
-        stage.setScene(scene);                          // indica qual cena utilizar no Stage
-    
-        stage.show();                                   // abrir o stage
-    
-    txtLogin.setLayoutX((pane.getWidth() - txtLogin.getWidth())/2);
-    txtLogin.setLayoutY(50);
-    
-    txtSenha.setLayoutX((pane.getWidth() - txtSenha.getWidth())/2);
-    txtSenha.setLayoutY(100);
-    
-    btnEntrar.setLayoutX((pane.getWidth() - btnEntrar.getWidth())/2);
-    btnEntrar.setLayoutY(150);
-    
-    btnSair.setLayoutX((pane.getWidth() - btnSair.getWidth())/2);
-    btnSair.setLayoutY(200);
-    /* CSS */
-    pane.setStyle("-fx-background-color: linear-gradient");
-            
-            //linear-gradient(from 0% 0% to 100& 100%, silver 100%);");
-    
+    private AnchorPane pane; // criar os componentes como variáveis globais
+    private TextField txtLogin;
+    private PasswordField txtSenha;
+    private Button btnEntrar, btnSair;
+    private static Stage stage; // variável global stage
+
+       
+    @Override
+    public void start(Stage stage) throws Exception {
         
+        initComponents();
+        initListeners();
+        
+        Scene scene = new Scene(pane);                  // cria a cena e passa o painel 'pane'
+        stage.setScene(scene);                          // indica qual cena utilizar no Stage
+        
+        stage.setResizable(false); // não maximizar a tela
+        stage.setTitle("Login - GolFX"); // título para a tela
+        
+        stage.show();    
+        initLayout();
+        
+        LoginApp.stage = stage; // indicar a variável global stage e seu método getter 
     }
     
-    public static void main (String [] args) {
+    public static Stage getStage() { //criar o getter da variável global stage
+        return stage;
+    }
+
+    private void initComponents() {
+
+        AnchorPane pane = new AnchorPane();             //  criar painel
+        pane.setPrefSize(400, 300);                      // double prefWidth double prefHeight
+        pane.setStyle("-fx-brackground-color: black;");
+        
+        TextField txtLogin = new TextField();          // cria caixa de texto
+        txtLogin.setPromptText("Digite aqui seu login!");
+
+        PasswordField txtSenha = new PasswordField();   // cria caixa de senha
+        txtSenha.setPromptText("Digite aqui sua senha!");
+
+        Button btnEntrar = new Button("Entrar");       // cria botão 
+
+        Button btnSair = new Button("Sair");           // cria botão
+
+        pane.getChildren().addAll(txtLogin, txtSenha, btnEntrar, btnSair); // adicionar os componentes no painel
+
+    }
+
+    private void initLayout() {
+
+        txtLogin.setLayoutX((pane.getWidth() - txtLogin.getWidth()) / 2);
+        txtLogin.setLayoutY(50);
+
+        txtSenha.setLayoutX((pane.getWidth() - txtSenha.getWidth()) / 2);
+        txtSenha.setLayoutY(100);
+
+        btnEntrar.setLayoutX((pane.getWidth() - btnEntrar.getWidth()) / 2);
+        btnEntrar.setLayoutY(150);
+
+        btnSair.setLayoutX((pane.getWidth() - btnSair.getWidth()) / 2);
+        btnSair.setLayoutY(200);
+    }
+
+    private void initListeners() {
+        btnSair.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                fecharAplicacao();
+            }
+        });
+        btnEntrar.setOnAction (new EventHandler<ActionEvent>(){
+
+            @Override
+            public void handle(ActionEvent event) {
+                logar();
+            }
+        
+    });
+    }
+
+    private void fecharAplicacao() {
+        System.exit(0);
+    }
+    private void logar(){
+        if (txtLogin.getText().equals("admin") &&
+        txtSenha.getText().equals("casadocodigo")) {
+        // TODO Abrir a tela VitrineApp
+        } else {
+        JOptionPane.showMessageDialog(null, "Login e/ou senha inválidos", "Erro", JOptionPane.ERROR_MESSAGE);
+        }
+
+    }
+    
+    
+    
+    
+    public static void main(String[] args) {
         launch(args);
     }
-    
-    
+
 }
